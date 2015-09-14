a3ceee2b448a19598ccaba26cb49b6d797982eed
Mike Brown <MBrown@1800contacts.com>
2015-09-14 12:30:22 -0600

    commit

--- towerDefense/Controllers/GameController.cs
+++ towerDefense/Controllers/GameController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
+using TowerDefense.Business;
+using TowerDefense.Business.Models;
+using TowerDefense.Interfaces;
 
 namespace towerDefense.Controllers
 {
@@ -12,6 +15,29 @@ namespace towerDefense.Controllers
         public ActionResult Index(string gameName)
         {
             return View("Index", (object)gameName);
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(string playername, string gamename, HttpPostedFileBase file)
+        {
+            byte[] data = new byte[file.InputStream.Length];
+            file.InputStream.Read(data, 0, data.Length);
+            var assembly = Assembly.Load(data);
+
+            var type = assembly.GetTypes().Single(t => t.GetInterfaces().Contains(typeof(ITower)));
+
+            var constructor = type.GetConstructor(new Type[] { });
+
+            var tower = (ITower) constructor.Invoke(new object[] { });
+
+            var game = GameManager.Games.Single(x => x.Name == gamename);
+            game.Players.Add(new Player
+            {
+                Name = playername,
+                Tower = tower
+            });
+
+            return RedirectToAction("../Game/" + gamename);
         }
     }
 }
