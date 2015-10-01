6bea54898b8a1d0ce0be1fe3c7e974afa17addf3
Mike Brown <MBrown@1800contacts.com>
2015-10-01 16:59:42 -0600

    small refactoring

--- TowerDefense.Business/Models/GameState.cs
+++ TowerDefense.Business/Models/GameState.cs
@@ -7,11 +7,14 @@ namespace TowerDefense.Business.Models
     public class GameState : IGameState
     {
         public List<IFoe> Foes { get; set; }
+        public List<IGoal> Goals { get; set; }
+        public List<ITank> Tanks { get; set; }
+
         public IEnumerable<IEntity> Entities
         {
-            get { return Foes.Select(foe => (IEntity)foe); }
+            get { return Foes.Select(foe => (IEntity)foe).Concat(Goals).Concat(Tanks); }
         }
+
         public Size Size { get; set; }
-        public List<IGoal> Goals { get; set; }
     }
 }
