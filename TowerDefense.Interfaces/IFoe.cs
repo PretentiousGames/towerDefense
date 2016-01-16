4f8ffd76f111087045273db47cdcddff1ef1ae25
Scott Stromberg <srstromberg@gmail.com>
2016-01-15 20:10:48 -0700

    exposed foe abilitytype

--- TowerDefense.Interfaces/IFoe.cs
+++ TowerDefense.Interfaces/IFoe.cs
@@ -5,6 +5,7 @@ namespace TowerDefense.Interfaces
     public interface IFoe : IEntity, IKillable
     {
         FoeType FoeType { get; }
+        AbilityType AbilityType { get; }
         double Speed { get; }
         double MaxSpeed { get; }
     }
